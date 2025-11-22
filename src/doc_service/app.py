5284aaab0d28c3b6d737b61099b3cfdbdc832da4
Sathvik <kotasathvik25@gmail.com>
2025-11-22 15:00:28 +0530

    building app.py fastAPI service in doc_service

---
+++ src/doc_service/app.py
@@ -0,0 +1,20 @@
+# app.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils import preprocess_documents
+
+app = FastAPI(title="Document Service")
+
+class FolderRequest(BaseModel):
+    folder: str
+
+@app.post("/load_docs")
+def load_docs(req: FolderRequest):
+    try:
+        docs = preprocess_documents(req.folder)
+        return {
+            "count": len(docs),
+            "documents": docs
+        }
+    except Exception as e:
+        return {"error": str(e)}
