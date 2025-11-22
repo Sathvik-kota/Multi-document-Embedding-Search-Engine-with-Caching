b53fcf04567d686ee091adba7d649e892205773f
Sathvik <kotasathvik25@gmail.com>
2025-11-22 17:54:37 +0530

    updating the function

--- src/embed_service/embedder.py
+++ src/embed_service/embedder.py
@@ -1,4 +1,4 @@
-# embedder.py
+# src/embed_service/embedder.py
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
@@ -8,15 +8,12 @@ class Embedder:
         self.model = SentenceTransformer(model_name)
 
     def embed_text(self, text: str):
-        """Embed a single document or query."""
         emb = self.model.encode(text, convert_to_numpy=True)
         return emb.astype("float32")
 
     def embed_batch(self, texts: list):
-        """Embed a batch of texts."""
         embs = self.model.encode(texts, convert_to_numpy=True)
         return embs.astype("float32")
 
     def dim(self):
-        """Embedding vector size."""
         return self.model.get_sentence_embedding_dimension()
